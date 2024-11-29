b73c6c935034a2a658491a82f69ae266a7c0ba12
Asamoahfrancis <brefofrancisasamaoh@gmail.com>
2024-11-29 21:59:58 +0000

    new socket connection

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("./socket/socket");
+// import "./socket/socket";
 const Server_1 = require("./NewExpress/Server");
 const dotenv_1 = __importDefault(require("dotenv"));
 const chalk_1 = __importDefault(require("chalk"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 8080;
-Server_1.app.listen(PORT, () => {
+const server = http_1.default.createServer(Server_1.app);
+Server_1.app.use((0, cors_1.default)());
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+    connectionStateRecovery: {},
+});
+Server_1.app.get("/health", (req, res) => {
+    res.status(200).send("Server is healthy");
+});
+io.on("connection", (socket) => {
+    console.log(chalk_1.default.green(`User connected: ${socket.id}`));
+    socket.on("chat message", (msg) => {
+        console.log(chalk_1.default.yellow(`Message received: ${msg}`));
+        io.emit("chat message", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log(chalk_1.default.red(`User disconnected: ${socket.id}`));
+    });
+});
+server.listen(PORT, () => {
     console.log(chalk_1.default.magenta(`Server listening on ${PORT}`));
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import "./socket/socket";
+// import "./socket/socket";
 import { app } from "./NewExpress/Server";
 import dotenv from "dotenv";
 import chalk from "chalk";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
 dotenv.config();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+  connectionStateRecovery: {},
+});
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy");
+});
+io.on("connection", (socket) => {
+  console.log(chalk.green(`User connected: ${socket.id}`));
+
+  socket.on("chat message", (msg) => {
+    console.log(chalk.yellow(`Message received: ${msg}`));
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(chalk.red(`User disconnected: ${socket.id}`));
+  });
+});
+
+server.listen(PORT, () => {
   console.log(chalk.magenta(`Server listening on ${PORT}`));
 });
